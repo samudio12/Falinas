607775c426661c2dca0eb446aeea1593b26d2f9d
Tiago Marques <tiagofm.profissional@outlook.pt>
2020-04-26 19:37:39 +0200

    hot rn carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,43 @@
 import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/all'
-import 'owl.carousel';
+import '@fortawesome/fontawesome-free/js/all';
+import { tns } from 'tiny-slider/src/tiny-slider.js';
 
-
-import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'tiny-slider/dist/tiny-slider.css';
 import '../scss/index.scss';
 
+$('#main-menu > button').click(() => {
+  if (
+    document.querySelector(
+      '#main-menu [aria-expanded="true"].navbar-toggler'
+    ) == null
+  ) {
+    console.log('open');
+    $('#main-menu').addClass('collapsed');
+  } else {
+    console.log('close');
+    $('#main-menu').removeClass('collapsed');
+  }
+});
 
-$('#main-menu > button').click( () => {
-    if(document.querySelector('#main-menu [aria-expanded="true"].navbar-toggler') == null){
-        console.log('open')
-        $('#main-menu').addClass('collapsed');
-
-    } else {
-        console.log('close')
-        $('#main-menu').removeClass('collapsed');
-
-    }
-})
-
+tns({
+  container: '.my-slider',
+  // items: 6,
+  slideBy: 'page',
+  autoplay: false,
+  gutter: 48,
+  mouseDrag: true,
+  responsive: {
+    640: {
+      //edgePadding: 20,
+      //gutter: 20,
+      items: 2,
+    },
+    700: {
+      //gutter: 30
+      items: 4,
+    },
+    900: {
+      items: 6,
+    },
+  },
+});
