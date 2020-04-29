e0662ab738927d2a6533e86d2bf892e685fc98b6
samudio12 <45163011+samudio12@users.noreply.github.com>
2020-04-30 00:26:51 +0200

    page2

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
 import { tns } from 'tiny-slider/src/tiny-slider.js';
+import noUiSlider from 'nouislider';
 
 import 'tiny-slider/dist/tiny-slider.css';
 import '../scss/index.scss';
+import 'nouislider/distribute/nouislider.css';
 
 $('#main-menu > button').click(() => {
   if (
@@ -19,11 +21,34 @@ $('#main-menu > button').click(() => {
   }
 });
 
+$(document).ready(function() {
+
+  const $valueSpan = $('.valueSpan2');
+  const $value = $('#customRange11');
+  $valueSpan.html($value.val());
+  $value.on('input change', () => {
+
+    $valueSpan.html($value.val());
+  });
+});
+
+
+$(document).ready(function() {
+  $('.mdb-select').materialSelect();
+  });
+
+
+  
+
 let carouselBaseConfig = {
-  slideBy: 'page',
-  autoplay: false,
+  slideBy: '1',
+  controls: false,
+  autoplayButtonOutput: false,
+  nav: false,
+  autoplay: true,
   gutter: 48,
   mouseDrag: true,
+  preventScrollOnTouch: 'auto',
   responsive: {
     640: {
       //edgePadding: 20,
